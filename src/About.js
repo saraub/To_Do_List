f83a2a2c92f9851700bf6813e597c09098d1eee3
Sara <67912060+saraub@users.noreply.github.com>
2020-08-04 18:31:10 +0200

    commit

---
+++ src/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function About(){
+
+    return(
+
+        <div>
+            <header className="App-header">
+                <p>here comes things about us!</p>
+
+
+
+            </header>
+        </div>
+    );
+}
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,146 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class Counter extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [
+        "Go to the store",
+        "Learn French",
+        "Call the office"
+        
+        
+      ],
+      done:[
+
+      ]
+      
+    }
+    this.addItem = this.addItem.bind(this);
+    this.removeItem = this.removeItem.bind(this);
+    
+  }
+  addItem(e) {
+    // Prevent button click from submitting form
+    e.preventDefault();
+
+    // Create variables for our list, the item to add, and our form
+    let list = this.state.list;
+    const newItem = document.getElementById("addInput");
+    const form = document.getElementById("addItemForm");
+
+    // If our input has a value
+    if (newItem.value !== "") {
+      // Add the new item to the end of our list array
+      list.push(newItem.value);
+      // Then we use that to set the state for list
+      this.setState({
+        list: list
+      });
+      // Finally, we need to reset the form
+      newItem.classList.remove("is-danger");
+      form.reset();
+    } else {
+      // If the input doesn't have a value, make the border red since it's required
+      newItem.classList.add("is-danger");
+    }
+  }
+  removeItem(item) {
+    // Put our list into an array
+    const list = this.state.list.slice();
+    // Check to see if item passed in matches item in array
+    list.some((el, i) => {
+      if (el === item) {
+        // If item matches, remove it from array
+        list.splice(i, 1);
+        return true;
+      }
+    });
+    // Set state to list
+    this.setState({
+      list: list,
+      
+
+    });
+    
+  }
+  doneitem(item){
+    const done =this.state.done;
+    done.push(item);
+    window.alert("Task have been done! Congrats!");
+    this.setState({
+      done: done
+    });
+    this.removeItem(item);
+
+  }
+  
+  
+  
+ 
+
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <div className="content">
+      <label>To Do List</label>
+  
+      <div className="container">
+        <section className="section">
+          <ul className=" card wrp">
+            {this.state.list.map(item => (
+            <li key={item}>
+            {item} &nbsp;
+            <span className="right-align">
+              <button className="delete btn-floating btn-large waves-effect waves-light red "
+              onClick={() => this.removeItem(item)}><i class="material-icons">clear</i>
+
+              </button>
+              <button className="delete btn-floating btn-large waves-effect waves-light green "
+              onClick={() => this.doneitem(item) }><i class="material-icons">check</i>
+
+              </button>
+
+            </span>
+            
+            </li>
+            ))}
+          </ul>
+        </section>
+        <hr />
+        <section className="section">
+          <form className="form" id="addItemForm">
+            <input
+              type="text"
+              className="input"
+              id="addInput"
+              placeholder="Something that needs ot be done..."
+            />
+            <button className="btn-floating btn-large waves-effect waves-light blue" onClick={this.addItem}>
+            <i class="material-icons">add</i>
+            </button>
+          </form>
+        </section>
+        
+      </div>
+      <div className="card-panel hoverable c">
+        <label>Tasks that have been completed</label>
+
+        <ul>
+          {this.state.done.map(item =>(<li key={item}>{item}</li>))}
+        </ul>
+
+      </div>
+      
+</div>
   );
+
 }
 
-export default App;
+}
+  
+  
+
+
+export default Counter;
